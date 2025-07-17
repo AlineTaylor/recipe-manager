b2eff1d873a17ea08b6d8a961f5bf2308a3e7e91
Aline Yui Taylor <alineyui.taylor@gmail.com>
2025-07-16 20:55:17 -0500

    Refactor app layout to integrate sidebar and main content; update header to accept drawer input for improved navigation and responsiveness.

--- recipe-manager/src/app/app.component.ts
+++ recipe-manager/src/app/app.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeaderComponent } from './shared/layout/header/header.component';
 import { FooterComponent } from './shared/layout/footer/footer.component';
 import { RecipeCardComponent } from './shared/layout/recipe-card/recipe-card.component';
 import { SharedModule } from './shared/shared.module';
+import { SidebarComponent } from './shared/layout/sidebar/sidebar.component';
+import { MatDrawer } from '@angular/material/sidenav';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
@@ -13,10 +16,28 @@ import { SharedModule } from './shared/shared.module';
     FooterComponent,
     DashboardComponent,
     RecipeCardComponent,
+    SidebarComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'recipe-manager';
+
+  @ViewChild('drawer') drawer!: MatDrawer;
+
+  isSmallScreen = false;
+
+  constructor(private breakpointObserver: BreakpointObserver) {
+    // listen for screen size changes
+    this.breakpointObserver
+      .observe([Breakpoints.Handset, Breakpoints.Tablet])
+      .subscribe((result) => {
+        this.isSmallScreen = result.matches;
+        // close the drawer optionally on mobile
+        if (this.drawer && this.isSmallScreen) {
+          this.drawer.close();
+        }
+      });
+  }
 }
--- recipe-manager/src/app/shared/layout/header/header.component.ts
+++ recipe-manager/src/app/shared/layout/header/header.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { SharedModule } from '../../shared.module';
-import { SidebarComponent } from '../sidebar/sidebar.component';
+import { MatDrawer } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [SharedModule, SidebarComponent],
+  imports: [SharedModule],
   templateUrl: './header.component.html',
   styleUrl: '../header-footer.component.css',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  @Input() drawer!: MatDrawer; // take drawer from parent
+}
