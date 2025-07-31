1b88f388ef5a6ceed23756d8156e5b4730c47b23
Aline Yui Taylor <alineyui.taylor@gmail.com>
2025-07-31 18:42:55 -0500

    Uncommented the "buggy" portion I commented out yesterday and guess what - it works now lol

--- recipe-manager/src/app/shared/utils/services/recipe.service.ts
+++ recipe-manager/src/app/shared/utils/services/recipe.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-// import { Observable } from 'rxjs';
-// import { environment } from '../../../../environments/environment';
-// import { Recipe } from '../recipe.model';
+import { Observable } from 'rxjs';
+import { environment } from '../../../../environments/environment';
+import { Recipe } from '../recipe.model';
 
 @Injectable({
   providedIn: 'root',
@@ -10,27 +10,27 @@ import { Injectable } from '@angular/core';
 export class RecipeService {
   constructor(private http: HttpClient) {}
 
-  // getRecipes(): Observable<Recipe[]> {
-  //   return this.http.get<Recipe[]>(`${environment.apiUrl}/recipes`);
-  // }
+  getRecipes(): Observable<Recipe[]> {
+    return this.http.get<Recipe[]>(`${environment.apiUrl}/recipes`);
+  }
 
-  // getRecipe(id: number): Observable<{ recipe: Recipe }> {
-  //   return this.http.get<{ recipe: Recipe }>(
-  //     `${environment.apiUrl}/recipes/${id}`
-  //   );
-  // }
+  getRecipe(id: number): Observable<{ recipe: Recipe }> {
+    return this.http.get<{ recipe: Recipe }>(
+      `${environment.apiUrl}/recipes/${id}`
+    );
+  }
 
-  // createRecipe(recipe: Recipe): Observable<Recipe> {
-  //   return this.http.post<Recipe>(`${environment.apiUrl}/recipes`, { recipe });
-  // }
+  createRecipe(recipe: Recipe): Observable<Recipe> {
+    return this.http.post<Recipe>(`${environment.apiUrl}/recipes`, { recipe });
+  }
 
-  // updateRecipe(id: number, recipe: Recipe): Observable<Recipe> {
-  //   return this.http.put<Recipe>(`${environment.apiUrl}/recipes/${id}`, {
-  //     recipe,
-  //   });
-  // }
+  updateRecipe(id: number, recipe: Recipe): Observable<Recipe> {
+    return this.http.put<Recipe>(`${environment.apiUrl}/recipes/${id}`, {
+      recipe,
+    });
+  }
 
-  // deleteRecipe(id: number): Observable<void> {
-  //   return this.http.delete<void>(`${environment.apiUrl}/recipes/${id}`);
-  // }
+  deleteRecipe(id: number): Observable<void> {
+    return this.http.delete<void>(`${environment.apiUrl}/recipes/${id}`);
+  }
 }
